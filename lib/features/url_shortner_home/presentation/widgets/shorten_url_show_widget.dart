f1bdac544eae0d4055e8d7098df352cb6e7060e9
DUDU0123 <sdu200115@gmail.com>
2024-10-19 22:54:17 +0530

    Some minor changes in the shorten url

--- lib/features/url_shortner_home/presentation/widgets/shorten_url_show_widget.dart
+++ lib/features/url_shortner_home/presentation/widgets/shorten_url_show_widget.dart
@@ -8,35 +8,35 @@ import 'package:url_shortner/features/url_shortner_home/presentation/bloc/bloc/u
 import 'package:url_shortner/features/url_shortner_home/presentation/utils/url_shortner_container_decoration.dart';
 
 Widget shortenUrlShowWidget({required String shortenUrl}) {
-    return Container(
-      decoration: commonContainerDecoration().copyWith(
-        boxShadow: [],
-        gradient: LinearGradient(colors: [
-          kWhite.withOpacity(0.2),
-          kWhite.withOpacity(0.2),
-        ]),
-      ),
-      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
-      width: screenWidth(context: navigatorKey.currentContext!),
-      child: Row(
-        children: [
-          Expanded(
-            child: TextWidgetCommon(
-              text: shortenUrl,
-              textColor: kWhite,
-              overflow: TextOverflow.ellipsis,
-            ),
+  return Container(
+    decoration: commonContainerDecoration().copyWith(
+      boxShadow: [],
+      gradient: LinearGradient(colors: [
+        kWhite.withOpacity(0.2),
+        kWhite.withOpacity(0.2),
+      ]),
+    ),
+    padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+    width: screenWidth(context: navigatorKey.currentContext!),
+    child: Row(
+      children: [
+        Expanded(
+          child: TextWidgetCommon(
+            text: 'https://quicktrim/${shortenUrl.substring(shortenUrl.length-4)}.com',
+            textColor: kWhite,
+            overflow: TextOverflow.ellipsis,
           ),
-          IconButton(
-            icon: const Icon(Icons.copy, color: Colors.white),
-            onPressed: () {
-              navigatorKey.currentContext!
-                  .read<UrlShortnerBloc>()
-                  .add(CopyToClipBoardEvent(shortenUrl: shortenUrl));
-            },
-            tooltip: 'Copy to clipboard',
-          ),
-        ],
-      ),
-    );
-  }
+        ),
+        IconButton(
+          icon: const Icon(Icons.copy, color: Colors.white),
+          onPressed: () {
+            navigatorKey.currentContext!
+                .read<UrlShortnerBloc>()
+                .add(CopyToClipBoardEvent(shortenUrl: shortenUrl));
+          },
+          tooltip: 'Copy to clipboard',
+        ),
+      ],
+    ),
+  );
+}
